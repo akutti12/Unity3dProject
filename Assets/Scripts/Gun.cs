527e261bed0a982a5b036fd6a1dc9ae5003bdc77
akutti12 <89026603+akutti12@users.noreply.github.com>
2023-12-03 12:22:03 +0200

    Project files

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,144 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class Gun : MonoBehaviour
+{
+ [SerializeField]
+    private bool AddBulletSpread = true;
+    [SerializeField]
+    private Vector3 BulletSpreadVariance = new Vector3(0.1f, 0.1f, 0.1f);
+    [SerializeField]
+    private ParticleSystem ShootingSystem;
+    [SerializeField]
+    private Transform BulletSpawnPoint;
+    [SerializeField]
+    private ParticleSystem ImpactParticleSystem;
+    [SerializeField]
+    private TrailRenderer BulletTrail;
+    [SerializeField]
+    public float ShootDelay = 0.5f;
+    [SerializeField]
+    private LayerMask Mask;
+    [SerializeField]
+    private float BulletSpeed = 100;
+    private Vector2 _currentRotation;
+    public float weaponSwayAmount = 10;
+    public float mouseSensitivity = 1;
+    public Vector3 normalLocalPosition;
+    public Vector3 aimingLocalPosition;
+        public float aimSmoothing = 10;
+
+
+    private Animator Animator;
+    private float LastShootTime;
+
+    private void Awake()
+    {
+        Animator = GetComponent<Animator>();
+    }
+
+    public void Shoot()
+    {
+        if (LastShootTime + ShootDelay < Time.time)
+        {
+            // Use an object pool instead for these! To keep this tutorial focused, we'll skip implementing one.
+            // For more details you can see: https://youtu.be/fsDE_mO4RZM or if using Unity 2021+: https://youtu.be/zyzqA_CPz2E
+
+            Animator.SetBool("IsShooting", true);
+            ShootingSystem.Play();
+            Vector3 direction = GetDirection();
+
+            if (Physics.Raycast(BulletSpawnPoint.position, direction, out RaycastHit hit, float.MaxValue, Mask))
+            {
+                TrailRenderer trail = Instantiate(BulletTrail, BulletSpawnPoint.position, Quaternion.identity);
+
+                StartCoroutine(SpawnTrail(trail, hit.point, hit.normal, true));
+
+                LastShootTime = Time.time;
+            }
+            // this has been updated to fix a commonly reported problem that you cannot fire if you would not hit anything
+            else
+            {
+                TrailRenderer trail = Instantiate(BulletTrail, BulletSpawnPoint.position, Quaternion.identity);
+
+                StartCoroutine(SpawnTrail(trail, BulletSpawnPoint.position + GetDirection() * 100, Vector3.zero, false));
+
+                LastShootTime = Time.time;
+            }
+        }
+    }
+
+    private void DetermineRotation()
+    {
+        Vector2 mouseAxis = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+        mouseAxis *= mouseSensitivity;
+        _currentRotation += mouseAxis;
+
+        _currentRotation.y = Mathf.Clamp(_currentRotation.y, -90, 90);
+
+        transform.localPosition += (Vector3)mouseAxis * weaponSwayAmount / 1000;
+
+        transform.root.localRotation = Quaternion.AngleAxis(_currentRotation.x, Vector3.up);
+        transform.parent.localRotation = Quaternion.AngleAxis(-_currentRotation.y, Vector3.right);
+
+
+    }
+
+    private Vector3 GetDirection()
+    {
+        Vector3 direction = transform.forward;
+
+        if (AddBulletSpread)
+        {
+            direction += new Vector3(
+                Random.Range(-BulletSpreadVariance.x, BulletSpreadVariance.x),
+                Random.Range(-BulletSpreadVariance.y, BulletSpreadVariance.y),
+                Random.Range(-BulletSpreadVariance.z, BulletSpreadVariance.z)
+            );
+
+            direction.Normalize();
+        }
+
+        return direction;
+    }
+
+    private void DetermineAim()
+    {
+        Vector3 target = normalLocalPosition;
+        if (Input.GetMouseButton(1)) 
+            target = aimingLocalPosition;
+
+        Vector3 desiredPosition = Vector3.Lerp(transform.localPosition, target, Time.deltaTime * aimSmoothing);
+
+        transform.localPosition = desiredPosition;
+    }
+
+    private IEnumerator SpawnTrail(TrailRenderer Trail, Vector3 HitPoint, Vector3 HitNormal, bool MadeImpact)
+    {
+        // This has been updated from the video implementation to fix a commonly raised issue about the bullet trails
+        // moving slowly when hitting something close, and not
+        Vector3 startPosition = Trail.transform.position;
+        float distance = Vector3.Distance(Trail.transform.position, HitPoint);
+        float remainingDistance = distance;
+
+        while (remainingDistance > 0)
+        {
+            Trail.transform.position = Vector3.Lerp(startPosition, HitPoint, 1 - (remainingDistance / distance));
+
+            remainingDistance -= BulletSpeed * Time.deltaTime;
+
+            yield return null;
+        }
+        Animator.SetBool("IsShooting", false);
+        Trail.transform.position = HitPoint;
+        if (MadeImpact)
+        {
+            Instantiate(ImpactParticleSystem, HitPoint, Quaternion.LookRotation(HitNormal));
+        }
+
+        Destroy(Trail.gameObject, Trail.time);
+    }
+}
---
+++ Assets/Scripts/GunController.cs
@@ -0,0 +1,186 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GunController : MonoBehaviour
+{
+   [Header("Gun Settings")]
+    public float fireRate = 0.1f;
+    public int clipSize = 30;
+    public int reservedAmmoCapacity = 270;
+    public Transform Spawnpoint;
+
+    //Variables that change throughout code
+    private bool _canShoot;
+    private int _currentAmmoInClip;
+    private int _ammoInReserve;
+    [SerializeField]
+    private TrailRenderer BulletTrail;
+    AudioSource Sound;
+    public AudioClip shootSound;
+
+
+    //Muzzleflash
+  
+
+    //Aiming
+    public Vector3 normalLocalPosition;
+    public Vector3 aimingLocalPosition;
+    
+
+
+    public float aimSmoothing = 10;
+
+    [Header("Mouse Settings")]
+    public float mouseSensitivity = 1;
+    private Vector2 _currentRotation;
+    public float weaponSwayAmount = 10;
+
+    //Weapon Recoil
+    public bool randomizeRecoil;
+    public Vector2 randomRecoilConstraints;
+    //You only need to assign this if randomize recoil is off
+    public Vector2[] recoilPattern;
+
+    private void Start()
+    {
+        _currentAmmoInClip = clipSize;
+        _ammoInReserve = reservedAmmoCapacity;
+        _canShoot = true;
+         
+        Sound = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        DetermineAim();
+        DetermineRotation();
+
+        if(Input.GetMouseButton(0) && _canShoot && _currentAmmoInClip > 0)
+        {
+            _canShoot = false;
+            _currentAmmoInClip--;
+            StartCoroutine(ShootGun());
+        }
+        else if(Input.GetKeyDown(KeyCode.R) && _currentAmmoInClip < clipSize && _ammoInReserve > 0)
+        {
+            int amountNeeded = clipSize - _currentAmmoInClip;
+            
+
+
+            if (amountNeeded >= _ammoInReserve)
+            {
+                _currentAmmoInClip += _ammoInReserve;
+                _ammoInReserve -= amountNeeded;
+            }
+            else
+            {
+                _currentAmmoInClip = clipSize;
+                _ammoInReserve -= amountNeeded;
+            }
+        }
+    }
+
+    private void DetermineRotation()
+    {
+        Vector2 mouseAxis = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+        mouseAxis *= mouseSensitivity;
+        _currentRotation += mouseAxis;
+
+        _currentRotation.y = Mathf.Clamp(_currentRotation.y, -90, 90);
+
+        transform.localPosition += (Vector3)mouseAxis * weaponSwayAmount / 1000;
+
+        transform.root.localRotation = Quaternion.AngleAxis(_currentRotation.x, Vector3.up);
+        transform.parent.localRotation = Quaternion.AngleAxis(-_currentRotation.y, Vector3.right);
+
+
+    }
+    private void DetermineAim()
+    {
+        Vector3 target = normalLocalPosition;
+        if (Input.GetMouseButton(1)) 
+            target = aimingLocalPosition;
+
+        Vector3 desiredPosition = Vector3.Lerp(transform.localPosition, target, Time.deltaTime * aimSmoothing);
+
+        transform.localPosition = desiredPosition;
+    }
+
+    private void DetermineRecoil()
+    {
+        transform.localPosition -= Vector3.forward * 0.1f;
+
+        if (randomizeRecoil)
+        {
+            float xRecoil = Random.Range(-randomRecoilConstraints.x, randomRecoilConstraints.x);
+            float yRecoil = Random.Range(-randomRecoilConstraints.y, randomRecoilConstraints.y);
+
+            Vector2 recoil = new Vector2(xRecoil, yRecoil);
+
+            _currentRotation += recoil;
+        }
+        else
+        {
+            int currentStep = clipSize + 1 - _currentAmmoInClip;
+            currentStep = Mathf.Clamp(currentStep, 0, recoilPattern.Length - 1);
+
+            _currentRotation += recoilPattern[currentStep];
+        }
+    }
+
+    private IEnumerator ShootGun()
+    {
+        DetermineRecoil();
+        
+
+        RaycastForEnemy();
+
+        yield return new WaitForSeconds(fireRate);
+        _canShoot = true;
+    }
+
+    
+
+    private void RaycastForEnemy()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(Spawnpoint.position, Spawnpoint.forward, out hit, 1 << LayerMask.NameToLayer("Enemy")))
+        {
+            Sound.PlayOneShot(shootSound);
+            TrailRenderer trail = Instantiate(BulletTrail, Spawnpoint.position, Quaternion.identity);
+            StartCoroutine(SpawnTrail(trail, hit));
+
+            try
+            {
+                Debug.Log("Hit an Enemy!");
+                Rigidbody rb = hit.transform.GetComponent<Rigidbody>();
+                rb.constraints = RigidbodyConstraints.None;
+                rb.AddForce(transform.parent.transform.forward * 500);
+            }
+            catch
+            {
+
+            }
+        }
+    }
+
+    private IEnumerator SpawnTrail(TrailRenderer Trail, RaycastHit Hit)
+    {
+        float time = 0;
+        Vector3 startPosition = Trail.transform.position;
+
+        while (time < 1)
+        {
+            Trail.transform.position = Vector3.Lerp(startPosition, Hit.point, time);
+            time += Time.deltaTime / Trail.time;
+
+            yield return null;
+        }
+        Trail.transform.position = Hit.point;
+    }
+
+
+}
